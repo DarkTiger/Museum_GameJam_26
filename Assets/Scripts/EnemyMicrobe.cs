502717ae29a12cc4f77c0f031add5af8af053b3a
Lor3nz14 <60523526+Lor3nz14@users.noreply.github.com>
2026-06-05 21:54:19 +0200

    Update enemy movement

--- Assets/Scripts/EnemyMicrobe.cs
+++ Assets/Scripts/EnemyMicrobe.cs
@@ -4,7 +4,9 @@ public class EnemyMicrobe : Microbe
 {
     Microbe target = null;
     Vector3 nextPosition = Vector3.zero;
-
+    float nextTimer = 0;
+    float timer = 0;
+    Vector2 timerLimit = new Vector2 (0.5f, 5f);
 
 
     void Update()
@@ -14,15 +16,32 @@ public class EnemyMicrobe : Microbe
 
     private void Move()
     {
-        if (nextPosition != Vector3.zero && transform.position != nextPosition)
+        if (timer <= nextTimer)
         {
-            transform.position += (transform.position - nextPosition).normalized * Speed * Time.deltaTime;
+            if (nextPosition != Vector3.zero && transform.position != nextPosition)
+            {
+                transform.position += (transform.position - nextPosition).normalized * Speed * Time.deltaTime;
+            }
+            else
+            {
+                nextPosition = new Vector3(Random.Range(-Screen.width, Screen.width), Random.Range(-Screen.height, Screen.height), 0f);
+            }
+            timer += Time.deltaTime;
         }
         else
         {
             nextPosition = new Vector3(Random.Range(-Screen.width, Screen.width), Random.Range(-Screen.height, Screen.height), 0f);
+            timer = 0;
+            nextTimer = NextTimer();
         }
+        
     }
+    private float NextTimer()
+    {
+        nextTimer = Random.Range(timerLimit.x, timerLimit.y);
+        return nextTimer;
+    }
+
 
 
 }
